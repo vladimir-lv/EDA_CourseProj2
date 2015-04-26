7bc389d4041eab115e4d5dfa1a2359ffb8225d26
vladimir-lv <vladimir.loiterstein@gmail.com>
2015-04-27 00:22:32 +0300

    minor changes

--- plot4.R
+++ plot4.R
@@ -11,13 +11,3 @@ png("plot4.png",width = 480, height = 480, units = "px")
 plot(nei.coal_agg$Emission~nei.coal_agg$year,type="l",xlab = "Year",ylab = "PM2.5 Emission",
       main="Emissions from coal combustion-related sources")
 dev.off()
-
-#5 
-nei.all <-NEI[NEI$fips %in% c("24510","06037"),]
-aaa<-merge(nei.all,SCC,by.x="SCC",by.y="SCC",all.x=TRUE)
-nei.vehicle<-aaa[grep("VEHICLE",toupper(aaa$Short.Name)),]
-nei.vehicle_agg<-aggregate(Emissions ~ fips+year, data=nei.vehicle, FUN=sum)
-nei.bltm<-nei.vehicle_agg[nei.vehicle_agg$fips=="24510",]
-nei.la<-nei.vehicle_agg[nei.vehicle_agg$fips=="06037",]
-plot(nei.la$Emission~nei.la$year,col="red",type="l")
-lines(nei.bltm$Emission~nei.bltm$year,col = "blue")
